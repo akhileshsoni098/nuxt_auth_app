c33cf0614c0c969baa6a19ef10689e9af78c69d4
easyryt <developer.easyryt@gmail.com>
2026-05-09 11:51:03 +0530

    mongodb and user fields interface and schema created and jwt token gentration inside utils also written

---
+++ nuxt-auth-app/server/models/User.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  age: number;
+  is_married: boolean;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    age: {
+      type: Number,
+    },
+    is_married: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
---
+++ nuxt-auth-app/server/utils/generateToken.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (id: string) => {
+  const config = useRuntimeConfig();
+
+  return jwt.sign({ _id: id }, config.jwtSecret);
+};
